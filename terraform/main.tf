ae0c29a41d0771eafc555140e321835b7f5db017
AnimeshA867 <animeshacharya867+github@gmail.com>
2026-08-13 11:27:43 +0545

    feat: enhance Terraform configuration with auto-generated Ansible inventory and additional variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.5.0"
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -16,7 +16,7 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  
+
   endpoints {
     ec2 = "http://localhost:4566"
     sts = "http://localhost:4566"
@@ -77,7 +77,7 @@ resource "aws_security_group" "docker_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
     from_port   = 3100
     to_port     = 3100
@@ -103,10 +103,36 @@ resource "aws_instance" "krypt_server" {
   instance_type = var.instance_type
   key_name      = aws_key_pair.krypt_key.key_name
   subnet_id     = aws_subnet.public_subnet.id
-  
+
   vpc_security_group_ids = [aws_security_group.docker_sg.id]
 
   tags = {
     Name = "krypt-microservices-host"
   }
+}
+
+
+# Auto-generate Ansible Inventory
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/inventory.tpl", {
+    instance_ip  = aws_instance.krypt_server.public_ip
+    instance_id  = aws_instance.krypt_server.id
+    ssh_key_path = var.ssh_key_path
+    ansible_user = var.ansible_user
+  })
+
+  filename = "${path.module}/../ansible/inventory.ini"
+
+  # Recreate file whenever instance changes
+  file_permission = "0644"
+}
+
+output "ansible_inventory_path" {
+  value       = local_file.ansible_inventory.filename
+  description = "Path to auto-generated Ansible inventory"
+}
+
+output "instance_public_ip" {
+  value       = aws_instance.krypt_server.public_ip
+  description = "Public IP of the Krypt server"
 }
